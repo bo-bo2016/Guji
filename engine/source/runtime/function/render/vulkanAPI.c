aa186cb4bdd5152ddde16d41945960952633768a
yyb <785817955@qq.com>
2023-05-04 01:09:59 +0800

    fix error

--- engine/source/runtime/function/render/vulkanAPI.c
+++ engine/source/runtime/function/render/vulkanAPI.c
@@ -1,5 +1,15 @@
 #include "function/render/vulkanAPI.h"
 
+const char* validationLayers[]={"VK_LAYER_KHRONOS_validation"};
+const char* deviceExtensions[]={"VK_KHR_SWAPCHAIN_EXTENSION_NAME"};
+VkInstance instance;
+VkSurfaceKHR surface;
+VkPhysicalDevice physicalDevice;
+VkDevice device;
+VkQueue graphicsQueue;
+VkQueue presentQueue;
+VkSwapchainKHR swapChain;
+
 void drawFrame(){
 
 }
--- engine/source/runtime/function/render/vulkanAPI.h
+++ engine/source/runtime/function/render/vulkanAPI.h
@@ -3,17 +3,6 @@
 
 #include "vulkan/vulkan.h"
 
-const char* validationLayers[]={"VK_LAYER_KHRONOS_validation"};
-const char* deviceExtensions[]={"VK_KHR_SWAPCHAIN_EXTENSION_NAME"};
-VkInstance instance;
-VkSurfaceKHR surface;
-VkPhysicalDevice physicalDevice;
-VkDevice device;
-VkQueue graphicsQueue;
-VkQueue presentQueue;
-VkSwapchainKHR swapChain;
-
-
 void initVulkan();
 void createInstance();
 void setupDebugMessager();
