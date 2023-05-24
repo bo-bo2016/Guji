33b97c3d006e74eaab6641c60db33e0a9ff16b37
bobo <785817955@qq.com>
2023-05-24 11:39:39 +0800

    vulkan

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include "engine/source/runtime/engine.h"
 #include <stdio.h>
+
 int main(int argc, char** argv)
 {
 	printf("start\n");
-    startEngine();
+	startEngine();
 	printf("end\n");
-    return 0;
+	return 0;
 }
